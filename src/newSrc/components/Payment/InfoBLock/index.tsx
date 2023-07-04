b8525ec9d6e99ca0ea7d78d406b8db3bc03fd30c
Zmeev06 <se.rg.ey06@icloud.com>
2023-07-04 22:48:01 +0300

    feat: add api & logic

--- src/newSrc/components/Payment/InfoBLock/index.tsx
+++ src/newSrc/components/Payment/InfoBLock/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import styles from './index.module.scss'
+import React, { useEffect, useState } from "react";
+import styles from "./index.module.scss";
+import { IOrderInfo } from "../../../interfaces/serviceInterfaces";
 
-const InfoBlock: React.FC = () => {
+
+interface InfoBlockProps {
+  id: number;
+  time: string;
+}
+const InfoBlock = ({id, time} : InfoBlockProps) => {
+  const [[m, s], setTime] = React.useState([3, 0]);
+  const [over, setOver] = React.useState(false);
+
+  const tick = () => {
+    if (m === 0 && s === 0) {
+      setOver(true);
+    } else if (s === 0) {
+      setTime([m - 1, 59]);
+    } else {
+      setTime([m, s - 1]);
+    }
+  };
+
+  React.useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerID);
+  });
+
   return (
     <div className={styles.main}>
       <div className={styles.number}>
         <p className={styles.title}>Номер сделки</p>
-        <p className={styles.value}>11ff11f</p>
+        <p className={styles.value}>{id}</p>
       </div>
       <div className={styles.time}>
         <p className={styles.title}>Время создания</p>
-        <p className={`${styles.value} ${styles.timeValue}`}>02.07.2023 13:19:22</p>
+        <p className={`${styles.value} ${styles.timeValue}`}>{time}</p>
       </div>
       <div className={styles.timer}>
-        <p className={styles.timerText}>2:10</p>
+        <p className={styles.timerText}>
+          {m}:{String(s).length === 1 ? `0${s}` : s}{" "}
+        </p>
       </div>
     </div>
-)
-}
-export default InfoBlock
+  );
+};
+export default InfoBlock;
